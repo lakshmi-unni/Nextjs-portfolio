9d446221499f40b4a2feca4c4cde339b47462e43
lakshmi-unni <lakshmiunnioffcial@gmail.com>
2025-02-07 02:36:55 +0530

    ui completed

--- app/components/SkillCard.jsx
+++ app/components/SkillCard.jsx
@@ -14,7 +14,7 @@ const SkillCard = ({
     whileInView={{opacity:1}}
     transition={{duration:0.8,delay:1}}
     // max-w-[768px] mx-auto"
-     className='mx-auto flex items-center gap-3 ring-2 ring-inset ring-zinc-50/10 rounded-2xl p-3 hover:-translate-y-1 duration-500 hover:shadow-md transition-transform group'>
+     className='flex items-center gap-3 ring-2 ring-inset ring-zinc-50/10 rounded-2xl p-3 hover:-translate-y-1 duration-500 hover:shadow-md transition-transform group'>
         <motion.figure
         whileHover={{scale:1.05}}
          className=' aspect-square border border-gray-400 rounded-lg overflow-hidden w-12 sm:w-14 p-2 cursor-pointer'>
