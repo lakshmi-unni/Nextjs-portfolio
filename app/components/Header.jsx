4d6074d0a6a701c468f5a0f78bb4222278a645a8
lakshmi-unni <lakshmiunnioffcial@gmail.com>
2025-02-09 20:40:53 +0530

    about margin changed

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -35,7 +35,7 @@ const Header = ({ isDarkMode }) => {
    whileInView={{opacity:1}}
    transition={{duration:0.8,delay:0.7}} 
   className='max-w-2xl mx-auto font-Ovo'>Frontend web developer with 3 years of expertise in React JS, Next JS. Specialized in building responsive, scalable web applications optimized for performance, accessibility, and SEO. Open to global opportunities.</motion.p>
-  <div className='flex flex-col sm:flex-row items-center gap-4 mt-4 mb-8'>
+  <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
     <motion.a
      initial={{y:30,opacity:0}}
      whileInView={{y:0,opacity:1}}
